040cf08fa5c30d391292a654380bc4fc1366f02e
arisa <arisankochira10@gmail.com>
2019-03-09 17:32:09 +0900

    商品登録する機能の作成、新規登録元呼び出しの作成、商品変更の作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,7 +9,21 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-  
+
   def create
+    @item = Item.new(item_params)
+
+    @item.save
+
+    redirect_to @item
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :price, :seller, :description, :email, :image_url)
   end
 end
